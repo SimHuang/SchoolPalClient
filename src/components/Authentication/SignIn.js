76a955e5fa5c0a8eeee331b93ffdf508c7fdf506
Simon Huang <simhuang92@gmail.com>
2017-06-01 01:55:15 -0400

    auth component styling

--- src/components/Authentication/SignIn.js
+++ src/components/Authentication/SignIn.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
+import style from '../../../style/components/SignIn.css'
 
 class SignIn extends Component {
 
     render() {
         return( 
-            <div>
-                <div>Sign in</div>
+            <div className={style.mainContainer}>
                 <div>
                     <h3>Sign In</h3>
                     <form>
-                        <input name="Username" type="text" placeholder="Username"/>
+                        <input className={style.field} name="Username" type="text" placeholder="Username"/>
                         <br/>
-                        <input name="Password" type="password" placeholder="Password" />
                         <br/>
-                        <button type="submit">Create Account</button>
+                        <input className={style.field} name="Password" type="password" placeholder="Password" />
+                        <br/>
+                        <br />
+                        <button className={style.submitBtn} type="submit">Log In</button>
+                        <button className={style.cancelBtn} type="submit">Cancel</button>
                     </form>
                 </div>
             </div>
--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,22 +1,29 @@
 import React, { Component } from 'react';
+import style from '../../../style/components/SignUp.css';
 
 class Signup extends Component {
     render() {
         return (
-            <div>
+            <div className={style.mainContainer}>
                 <h3>Create Account</h3>
                 <form>
-                    <input name="Email" type="text" placeholder="Email"/>
+                    <input className={style.field} name="Email" type="text" placeholder="Email"/>
                     <br/>
-                    <input name="Firstname" type="text" placeholder="Firstname"/>
                     <br/>
-                    <input name="Lastname" type="text" placeholder="Lastname"/>
+                    <input className={style.field} name="Firstname" type="text" placeholder="Firstname"/>
                     <br/>
-                    <input name="Username" type="text" placeholder="Username"/>
                     <br/>
-                    <input name="Password" type="password" placeholder="Password" />
+                    <input className={style.field} name="Lastname" type="text" placeholder="Lastname"/>
                     <br/>
-                    <button type="submit">Create Account</button>
+                    <br/>
+                    <input className={style.field} name="Username" type="text" placeholder="Username"/>
+                    <br/>
+                    <br/>
+                    <input className={style.field} name="Password" type="password" placeholder="Password" />
+                    <br/>
+                    <br/>
+                    <button className={style.submitBtn} type="submit">Create Account</button>
+                    <button className={style.cancelBtn} >Cancel</button>
                 </form>
             </div>
         )
