52a34d2862044027047b4b2d93e9175681673e23
Simon Huang <simhuang92@gmail.com>
2017-12-08 23:18:35 -0500

    style answer post component

--- src/components/Modal/AnswerPost.js
+++ src/components/Modal/AnswerPost.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-// import style from '../../style/components/AnswerPost.css';
+import style from '../../../style/components/AnswerPost.css';
 import { connect } from 'react-redux';
 import classNames from 'classnames';
 
 import ModalWrapper from './ModalWrapper';
+
+import { Button, Form, TextArea } from 'semantic-ui-react';
 
 /**
  * This is the component where the user can answer a post question
  */
 class AnswerPost extends Component {
 
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillUnmount() {
+        //make sure no data is left hanging
+    }
+
     render() {
+        var answerPostClass = classNames(
+            'ui',
+            'primary',
+            'button',
+            style.custom,
+        );
+
         return (
             <ModalWrapper {...this.props}>
-                <div>
-                    <div>
-                        This is the answer post
-                    </div>
-                </div>
+                <Form>  
+                    <TextArea placeholder="What do you have in mind?" style={{ minHeight: 300, marginBottom: '10px' }}/>
+                    <Button className={answerPostClass} role="button" content='Submit Answer'/>
+                </Form>
             </ModalWrapper>
         )
-        
     }
 }
 
