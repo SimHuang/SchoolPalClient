f3fbe253ab3c4d3fa61e3240903125bd6deb8b49
Simon Huang <simhuang92@gmail.com>
2017-12-26 23:25:39 -0500

    clear redux store on signout

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,14 +4,24 @@ import reducerAuth from './reducerAuth';
 import reducerThread from './reducerThread';
 import reducerModal from './reducerModal';
 import { reducer as formReducer } from 'redux-form';
+import { UNAUTH_USER } from '../actions/types';
 
-
-const rootReducer = combineReducers({
+//the default combined reducer to return to user unless user logs out
+const appReducer = combineReducers({
     auth: reducerAuth,  //state holding the authentication
     post: reducerPost,  //state to hold the posts
     form: formReducer,   //redux form
     thread: reducerThread, //holds the state for a post thread
     modal: reducerModal, //this just holds a single variable indication which modal is up initally null
 });
+
+const rootReducer = (state, action) => {
+    //clear the state if user logs out
+    if(action.type === UNAUTH_USER) {
+        state = undefined
+    }
+
+    return appReducer(state, action);
+};
 
 export default rootReducer;
