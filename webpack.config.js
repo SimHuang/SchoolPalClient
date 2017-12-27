493c2e0eddaae730e1c50340d067f05f3fb7fd9e
Simon Huang <simhuang92@gmail.com>
2017-12-26 19:38:45 -0500

    updating building files

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          presets: ['react', 'env', 'es2015', 'stage-1']
         }
       },
       {
